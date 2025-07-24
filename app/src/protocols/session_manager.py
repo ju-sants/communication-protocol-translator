0e661de8cc021cfc0f6d01661ea8c648748c1dc8
juan <juan.bisposantos05@gmail.com>
2025-07-24 15:05:57 -0300

    adicionando funcoes para remover um rastreador conectado e para obter sua conexão

--- app/src/protocols/session_manager.py
+++ app/src/protocols/session_manager.py
@@ -5,7 +5,7 @@ from app.core.logger import get_logger
 
 logger = get_logger(__name__)
 
-class TrackerSessionManager:
+class TrackerSessionsManager:
     _instance = None
     _lock = threading.Lock()
 
@@ -17,7 +17,20 @@ class TrackerSessionManager:
 
         return cls._instance
     
-    def register_tracker(self, dev_id: str, conn: socket.socket):
+    def register_tracker_client(self, dev_id: str, conn: socket.socket):
         with self._lock:
             self._active_clients[dev_id] = conn
             logger.info(f"Rastreador registrado na sessão: dev_id={dev_id}")
+
+    def remove_tracker_client(self, dev_id: str):
+        with self._lock:
+            if dev_id in self.active_trackers:
+                del self.active_trackers[dev_id]
+
+                logger.info(f"Rastreador removido de sua sessão: dev_id={dev_id}")
+    
+    def get_tracker_client_socket(self, dev_id: str):
+        with self._lock:
+            return self.active_trackers.get(dev_id)
+
+tracker_sessions_manager = TrackerSessionsManager()
