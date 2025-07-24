03c723bea8ec3fda38e43b3cfa087facdb481e47
juan <juan.bisposantos05@gmail.com>
2025-07-24 17:45:39 -0300

    Criando funcao para calcular a CRC TABLE, que é uma tabela que calcula 256 valores possíveis de um byte divididos por um polinõmio comum, essa parte da integração se mostrou ser mais complexa que o imaginado, após essa funcao precomputar esses resultados, podemos numa próxima verificar o valor de um check de segurança apenas verificando o valor correspomdente do bit na sua posicao atual dentro da tabela CRC, é como um complexo gabarito...

---
+++ app/src/protocols/gt06/processor.py
@@ -0,0 +1,31 @@
+import struct
+
+from . import builder, mapper
+from app.core.logger import get_logger
+
+
+logger = get_logger(__name__)
+
+CRC_TABLE = []
+
+def precompute_crc_table():
+    global CRC_TABLE
+
+    if len(CRC_TABLE) == 256:
+        return
+    
+    polynomial = 0x1021
+    for i in range(256):
+        crc = 0
+        c = i << 8
+
+        for j in range(8):
+            if (crc ^ c) & 0x8000:
+                crc = (crc << 1) ^ polynomial
+            else:
+                crc = crc << 1
+            
+            c = c << 1
+        CRC_TABLE.append(crc & 0xFFFF)
+
+precompute_crc_table()
