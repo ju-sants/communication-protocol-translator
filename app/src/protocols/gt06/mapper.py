7cc64db88057cec246c6178fc0e7a7110b416867
juan <juan.bisposantos05@gmail.com>
2025-07-25 09:30:47 -0300

    add salvamento da informação de power status

--- app/src/protocols/gt06/mapper.py
+++ app/src/protocols/gt06/mapper.py
@@ -120,9 +120,13 @@ def handle_heartbeat_packet(dev_id_str: str, serial: int, body: bytes):
     # O pacote de Heartbeat (0x13) contém informações de status
     terminal_info = body[0]
     acc_status = terminal_info & 0b10 
-    
+
+    power_alarm_flag = (terminal_info >> 3) & 0b111
+    current_power_status = 1 if power_alarm_flag == 0b010 else 0 # 1 = desconectado
+
     # Atualiza o status da ignição no Redis
     redis_client.hset(dev_id_str, 'acc_status', 1 if acc_status else 0)
+    redis_client.hset(dev_id_str, "power_status", current_power_status)
 
     # Keep-Alive da Suntech
     suntech_packet = build_suntech_alv_packet(dev_id_str)
