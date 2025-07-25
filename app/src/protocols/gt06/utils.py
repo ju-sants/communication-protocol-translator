5fca91b124455b3a89cb8055fdea28dcd810b038
juan <juan.bisposantos05@gmail.com>
2025-07-25 10:09:17 -0300

    alocando funcoes de calculo de CRC itu em utils.py

---
+++ app/src/protocols/gt06/utils.py
@@ -0,0 +1,30 @@
+CRC_TABLE = []
+
+def precompute_crc_table():
+    global CRC_TABLE
+
+    if len(CRC_TABLE) == 256:
+        return
+    
+    polynomial = 0x1021
+    for i in range(256):
+        crc = 0
+        c = i << 8
+
+        for j in range(8):
+            if (crc ^ c) & 0x8000:
+                crc = (crc << 1) ^ polynomial
+            else:
+                crc = crc << 1
+            
+            c = c << 1
+        CRC_TABLE.append(crc & 0xFFFF)
+
+precompute_crc_table()
+
+
+def crc16_itu(data: bytes) -> int:
+    crc = 0
+    for byte in data:
+        crc = ((crc << 8) & 0xFF00) ^ CRC_TABLE[((crc >> 8) & 0xFF) ^ byte]
+    return crc & 0xFFFF
