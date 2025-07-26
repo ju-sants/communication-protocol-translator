9fabc00ca98f2cc908166592e1ffa66771bbb8f7
juan <juan.bisposantos05@gmail.com>
2025-07-26 09:08:02 -0300

    adicionando log formatado para visualização humana com a funcao format_gt06_packet_for_display

--- app/src/protocols/gt06/handler.py
+++ app/src/protocols/gt06/handler.py
@@ -2,6 +2,8 @@
 import socket
 from app.core.logger import get_logger
 from .processor import process_packet
+from .utils import format_gt06_packet_for_display 
+
 from app.src.protocols.session_manager import tracker_sessions_manager
 from app.services.redis_service import get_redis
 
@@ -43,6 +45,9 @@ def handle_connection(conn: socket.socket, addr):
                         # Corpo do pacote que vai para o processador: [Length(1) + Proto(1) + Conteúdo + Serial(2) + CRC(2)]
                         packet_body = raw_packet[2:-2]
                         
+                        # Formatando pacote para display
+                        logger.info(f"Pacote Formatado Recebido de {addr}:\n{format_gt06_packet_for_display(packet_body)}")
+
                         # Chama o processador, passando o ID da sessão
                         response_packet, newly_logged_in_dev_id = process_packet(dev_id_session, packet_body)
                         
