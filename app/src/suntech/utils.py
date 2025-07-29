157babce84a4c7437887e2e542f63cb7cc7134fc
juan <juan.bisposantos05@gmail.com>
2025-07-29 13:57:48 -0300

    tentando obter primeiramente a voltagem dos dados de localização, e como fallback o valor padrão 12.43

--- app/src/suntech/utils.py
+++ app/src/suntech/utils.py
@@ -53,7 +53,7 @@ def build_suntech_packet(hdr: str, dev_id: str, location_data: dict, serial: int
     
     telemetry_fields = [
         assign_map,
-        "12.43", # PWR_VOLT
+        str(location_data.get("voltage", "12.43")), # PWR_VOLT
         "0.0",   # BCK_VOLT
         str(int(location_data.get('gps_odometer', 0))), # GPS_ODOM
         "69647"  # H_METER
