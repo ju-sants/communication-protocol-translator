a4f33dd3b86cd4de1517bb8a3e61c907d897d3fd
juan <juan.bisposantos05@gmail.com>
2025-08-20 10:36:45 -0300

    Alterando loggings para refletirem ao NT40

--- app/src/protocols/nt40/processor.py
+++ app/src/protocols/nt40/processor.py
@@ -8,13 +8,13 @@ logger = get_logger(__name__)
 
 def process_packet(dev_id_str: str | None, packet_body: bytes) -> tuple[bytes | None, str | None]:
     """
-    Processa o corpo de um pacote GT06, valida, disseca e delega a ação.
+    Processa o corpo de um pacote NT40, valida, disseca e delega a ação.
     Recebe o dev_id da sessão (se já conhecido).
     Retorna uma tupla: (pacote_de_resposta, dev_id_extraido_do_login)
     """
     # Validação Mínima de Tamanho
     if len(packet_body) < 6:
-        logger.warning(f"Pacote GT06 recebido muito curto para processar: {packet_body.hex()}")
+        logger.warning(f"Pacote NT40 recebido muito curto para processar: {packet_body.hex()}")
         return None, None
 
     # CRC
@@ -23,7 +23,7 @@ def process_packet(dev_id_str: str | None, packet_body: bytes) -> tuple[bytes | 
     calculated_crc = utils.crc_itu(data_to_check)
 
     if received_crc != calculated_crc:
-        logger.warning(f"Checksum GT06 inválido! pacote={packet_body.hex()}, crc_recebido={hex(received_crc)}, crc_calculado={hex(calculated_crc)}")
+        logger.warning(f"Checksum NT40 inválido! pacote={packet_body.hex()}, crc_recebido={hex(received_crc)}, crc_calculado={hex(calculated_crc)}")
         return None, None
     
     protocol_number = packet_body[1]
@@ -38,35 +38,35 @@ def process_packet(dev_id_str: str | None, packet_body: bytes) -> tuple[bytes | 
         newly_logged_in_dev_id = imei_bytes.hex()
         response_packet = builder.build_generic_response(protocol_number, serial_number)
     
-    elif protocol_number in [0x12, 0x22, 0xA0, 0x32]: # Pacotes de Localização
+    elif protocol_number in [0x12, 0x22]: # Pacotes de Localização
         if dev_id_str:
             mapper.handle_location_packet(dev_id_str, serial_number, content_body, protocol_number, packet_body.hex())
         else:
-            logger.warning(f"Pacote de localização GT06 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
+            logger.warning(f"Pacote de localização NT40 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
         response_packet = None
 
     elif protocol_number == 0x13: # Pacote de Heartbeat/Status
         if dev_id_str:
             mapper.handle_heartbeat_packet(dev_id_str, serial_number, content_body, packet_body.hex())
         else:
-            logger.warning(f"Pacote de heartbeat GT06 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
+            logger.warning(f"Pacote de heartbeat NT40 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
         response_packet = builder.build_generic_response(protocol_number, serial_number)
 
     elif protocol_number == 0x16: # Pacote de Alarme
         if dev_id_str:
             mapper.handle_alarm_packet(dev_id_str, serial_number, content_body, packet_body.hex())
         else:
-            logger.warning(f"Pacote de alarme GT06 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
+            logger.warning(f"Pacote de alarme NT40 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
         response_packet = builder.build_generic_response(protocol_number, serial_number)
     
     elif protocol_number == 0x15:
         if dev_id_str:
             mapper.handle_reply_command_packet(dev_id_str, serial_number, content_body, packet_body.hex())
         else:
-            logger.warning(f"Pacote de reply command GT06 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
+            logger.warning(f"Pacote de reply command NT40 recebido antes do login. Ignorando. pacote={packet_body.hex()}")
 
     else:
-        logger.warning(f"Protocolo GT06 não mapeado: {hex(protocol_number)} device_id={dev_id_str}")
+        logger.warning(f"Protocolo NT40 não mapeado: {hex(protocol_number)} device_id={dev_id_str}")
         response_packet = builder.build_generic_response(protocol_number, serial_number)
 
     return (response_packet, newly_logged_in_dev_id)
