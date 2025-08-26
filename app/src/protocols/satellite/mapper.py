16eea7b4ef2e9f7b5029d6d7d4dd6fafffca4aed
juan <juan.bisposantos05@gmail.com>
2025-08-26 11:12:11 -0300

    Recuperando ID do rastreador GSM com que esse satelital faz par, juntando seus pacotes de localização e alterando apenas as particularidades do satelital, depois construimos o pacote e enviamos pelo ID do GSM

--- app/src/protocols/satellite/mapper.py
+++ app/src/protocols/satellite/mapper.py
@@ -26,6 +26,33 @@ def map_data(raw_data: str):
         
         logger.info(f"Parsed JSON data: {data}")
 
+        hybrid_gsm = redis_client.hget("SAT_GSM_MAPPING", esn)
+
+        if not hybrid_gsm:
+            logger.info("Satellite tracker without a GSM pair, dropping.")
+            return
+        
+        last_serial = redis_client.hget(hybrid_gsm, "last_serial")
+        last_serial = last_serial if last_serial else 0
+
+        last_gsm_location_str = redis_client.hget(hybrid_gsm, "last_location_data")
+
+        last_gsm_location = json.loads(last_gsm_location_str)
+
+        last_hybrid_location = {**last_gsm_location, **data}
+        last_hybrid_location["voltage"] = 2.22
+        last_hybrid_location["satellites"] = 2
+
+        packet = build_suntech_packet(
+            "STT",
+            hybrid_gsm,
+            last_hybrid_location,
+            last_serial,
+            False,
+        )
+
+        send_to_main_server(hybrid_gsm, last_serial, packet.encode("ascii"), raw_data.encode("utf-8"))
+
     except json.JSONDecodeError as e:
         logger.error(f"Failed to decode JSON: {e}")
     except Exception as e:
