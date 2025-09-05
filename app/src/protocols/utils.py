17af1ad739cd1ae401b6e8ed559a86a8f10abb8c
juan <juan.bisposantos05@gmail.com>
2025-09-05 09:24:54 -0300

    deprecado...

--- app/src/protocols/utils.py
+++ app/src/protocols/utils.py
@@ -58,7 +58,7 @@ def handle_ignition_change(dev_id_str: str, serial, location_data: dict, raw_pac
         logger.exception(f"Erro ao processar mudança de ignição para device_id={dev_id_str}")
 
 
-# DEPRECATED
+# DEPRECATED, MANTEINED BY SUPPORT TO JT808 PROTOCOL
 def handle_power_change(dev_id_str: str, serial, location_data: dict):
     """Verifica se houve mudança no status da alimentação e envia o alerta correspondente."""
     try:
@@ -75,16 +75,16 @@ def handle_power_change(dev_id_str: str, serial, location_data: dict):
                 # Mudou de Conectado (0) para Desconectado (1)
                 alert_id = SUNTECH_POWER_DISCONNECTED_ALERT_ID # Alerta 41
                 logger.info(f"EVENTO DETECTADO: Alimentação Principal Desconectada para device_id={dev_id_str}")
-                power_alert_packet = build_suntech_packet(
-                    "ALT", dev_id_str, location_data, serial, is_realtime=True, alert_id=alert_id
-                )
+                # power_alert_packet = build_suntech_packet(
+                #     "ALT", dev_id_str, location_data, serial, is_realtime=True, alert_id=alert_id
+                # )
             else:
                 # Mudou de Desconectado (1) para Conectado (0)
                 alert_id = SUNTECH_POWER_CONNECTED_ALERT_ID # Alerta 40
                 logger.info(f"EVENTO DETECTADO: Alimentação Principal Conectada para device_id={dev_id_str}")
-                power_alert_packet = build_suntech_packet(
-                    "ALT", dev_id_str, location_data, serial, is_realtime=True, alert_id=alert_id
-                )
+                # power_alert_packet = build_suntech_packet(
+                #     "ALT", dev_id_str, location_data, serial, is_realtime=True, alert_id=alert_id
+                # )
             
             if power_alert_packet:
                 send_to_main_server(dev_id_str, serial, power_alert_packet.encode('ascii'))
