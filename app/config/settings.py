58bcc53cce7046578b79ac61c80b03739f8507a6
juan <juan.bisposantos05@gmail.com>
2025-09-05 11:58:32 -0300

    Criando mais uma categoria das funcoes construtoras: "alert"

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,7 +3,7 @@ from typing import Dict, Any
 import os
 
 from app.src.output.suntech.utils import (
-    build_location_packet as build_suntech_location_packet,
+    build_location_packet as build_suntech_alert_location_packet,
     build_heartbeat_packet as build_suntech_heartbeat_packet, 
     build_reply_packet as build_suntech_reply_packet
 )
@@ -11,7 +11,8 @@ from app.src.output.suntech.utils import (
 from app.src.output.gt06.utils import (
     build_location_packet as build_gt06_location_packet,
     build_heartbeat_packet as build_gt06_heartbeat_packet,
-    build_reply_packet as build_gt06_reply_packet
+    build_reply_packet as build_gt06_reply_packet,
+    build_alarm_packet as build_gt06_alarm_packet
 )
 
 from app.src.protocols.jt808.builder import process_suntech_command as process_suntech_command_to_jt808
@@ -97,12 +98,14 @@ class Settings(BaseSettings):
     # ---------- Utilitários para os protocolos de saída --------------------
     OUTPUT_PROTOCOL_PACKET_BUILDERS: Dict[str, Dict[str, Any]] = {
         "suntech": {
-            "location": build_suntech_location_packet,
+            "location": build_suntech_alert_location_packet,
+            "alert": build_suntech_alert_location_packet,
             "heartbeat": build_suntech_heartbeat_packet,
             "command_reply": build_suntech_reply_packet
             },
         "gt06": {
             "location": build_gt06_location_packet,
+            "alert": build_gt06_alarm_packet,
             "heartbeat": build_gt06_heartbeat_packet,
             "command_reply": build_gt06_reply_packet
             }
