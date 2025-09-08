6b2653a0b2df2e66461355d7a35ce1eeaca75584
juan <juan.bisposantos05@gmail.com>
2025-09-08 09:46:37 -0300

    Separando configurações dos protocolos de saída para evitar circular imports

---
+++ app/config/output_protocol_settings.py
@@ -0,0 +1,54 @@
+from pydantic_settings import BaseSettings
+from typing import Dict, Any
+import os
+
+from app.src.output.suntech.builder import (
+    build_location_alarm_packet as build_suntech_alert_location_packet,
+    build_heartbeat_packet as build_suntech_heartbeat_packet, 
+    build_reply_packet as build_suntech_reply_packet
+)
+
+from app.src.output.gt06.builder import (
+    build_location_packet as build_gt06_location_packet,
+    build_heartbeat_packet as build_gt06_heartbeat_packet,
+    build_reply_packet as build_gt06_reply_packet,
+    build_alarm_packet as build_gt06_alarm_packet
+)
+
+# from app.src.protocols.jt808.builder import process_suntech_command as process_suntech_command_to_jt808
+from app.src.protocols.gt06.builder import process_suntech_command as process_suntech_command_to_gt06
+from app.src.protocols.vl01.builder import process_suntech_command as process_suntech_command_to_vl01
+from app.src.protocols.nt40.builder import process_suntech_command as process_suntech_command_to_nt40
+
+class OutputProtocolSettings(BaseSettings):
+    # ---------- Utilitários para os protocolos de saída --------------------
+    OUTPUT_PROTOCOL_PACKET_BUILDERS: Dict[str, Dict[str, Any]] = {
+        "suntech": {
+            "location": build_suntech_alert_location_packet,
+            "alert": build_suntech_alert_location_packet,
+            "heartbeat": build_suntech_heartbeat_packet,
+            "command_reply": build_suntech_reply_packet
+            },
+        "gt06": {
+            "location": build_gt06_location_packet,
+            "alert": build_gt06_alarm_packet,
+            "heartbeat": build_gt06_heartbeat_packet,
+            "command_reply": build_gt06_reply_packet
+            }
+    }
+
+    OUTPUT_PROTOCOL_COMMAND_PROCESSORS: Dict[str, Dict[str, Any]] = {
+        "suntech": {
+            # "jt808": process_suntech_command_to_jt808,
+            "gt06": process_suntech_command_to_gt06,
+            "vl01": process_suntech_command_to_vl01,
+            "nt40": process_suntech_command_to_nt40
+        }
+    }
+
+    OUTPUT_PROTOCOL_HOST_ADRESSES: Dict[str, tuple] = {
+        "suntech": (os.getenv("SUNTECH_MAIN_SERVER_HOST"), os.getenv("SUNTECH_MAIN_SERVER_PORT")),
+        "gt06": (os.getenv("GT06_MAIN_SERVER_HOST"), os.getenv("GT06_MAIN_SERVER_PORT"))
+    }
+
+output_protocol_settings = OutputProtocolSettings()
--- app/config/settings.py
+++ app/config/settings.py
@@ -1,24 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Dict, Any
-import os
 
-from app.src.output.suntech.builder import (
-    build_location_alarm_packet as build_suntech_alert_location_packet,
-    build_heartbeat_packet as build_suntech_heartbeat_packet, 
-    build_reply_packet as build_suntech_reply_packet
-)
-
-from app.src.output.gt06.builder import (
-    build_location_packet as build_gt06_location_packet,
-    build_heartbeat_packet as build_gt06_heartbeat_packet,
-    build_reply_packet as build_gt06_reply_packet,
-    build_alarm_packet as build_gt06_alarm_packet
-)
-
-# from app.src.protocols.jt808.builder import process_suntech_command as process_suntech_command_to_jt808
-from app.src.protocols.gt06.builder import process_suntech_command as process_suntech_command_to_gt06
-from app.src.protocols.vl01.builder import process_suntech_command as process_suntech_command_to_vl01
-from app.src.protocols.nt40.builder import process_suntech_command as process_suntech_command_to_nt40
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -65,7 +47,7 @@ class Settings(BaseSettings):
     }
 
      # -------------- Dicionários Globais Para IDs de Alerta ----------------------
-    GLOBAL_ALERT_ID_DICTIONARY = {
+    GLOBAL_ALERT_ID_DICTIONARY: Dict[str, Dict[int, int]] = {
         "suntech": {
             1: 6501, 3: 6503, 4: 6504, 5: 6505, 6: 6506, 14: 6514, 15: 6515, 33: 6533, 34: 6534,
             41: 6541, 42: 6542, 46: 6546, 47: 6547, 73: 6573, 147: 6647
@@ -89,40 +71,9 @@ class Settings(BaseSettings):
     }
 
     # Mapeamento reverso para consultas mais rápidas
-    REVERSE_GLOBAL_ALERT_ID_DICTIONARY = {
+    REVERSE_GLOBAL_ALERT_ID_DICTIONARY: Dict[str, Dict[int, int]] = {
         protocol: {v: k for k, v in alerts.items()}
         for protocol, alerts in GLOBAL_ALERT_ID_DICTIONARY.items()
-    }
-
-
-    # ---------- Utilitários para os protocolos de saída --------------------
-    OUTPUT_PROTOCOL_PACKET_BUILDERS: Dict[str, Dict[str, Any]] = {
-        "suntech": {
-            "location": build_suntech_alert_location_packet,
-            "alert": build_suntech_alert_location_packet,
-            "heartbeat": build_suntech_heartbeat_packet,
-            "command_reply": build_suntech_reply_packet
-            },
-        "gt06": {
-            "location": build_gt06_location_packet,
-            "alert": build_gt06_alarm_packet,
-            "heartbeat": build_gt06_heartbeat_packet,
-            "command_reply": build_gt06_reply_packet
-            }
-    }
-
-    OUTPUT_PROTOCOL_COMMAND_PROCESSORS: Dict[str, Dict[str, Any]] = {
-        "suntech": {
-            # "jt808": process_suntech_command_to_jt808,
-            "gt06": process_suntech_command_to_gt06,
-            "vl01": process_suntech_command_to_vl01,
-            "nt40": process_suntech_command_to_nt40
-        }
-    }
-
-    OUTPUT_PROTOCOL_HOST_ADRESSES = {
-        "suntech": (os.getenv("SUNTECH_MAIN_SERVER_HOST"), os.getenv("SUNTECH_MAIN_SERVER_PORT")),
-        "gt06": (os.getenv("GT06_MAIN_SERVER_HOST"), os.getenv("GT06_MAIN_SERVER_PORT"))
     }
 
 settings = Settings()
