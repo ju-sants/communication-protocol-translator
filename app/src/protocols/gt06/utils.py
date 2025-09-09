04de57a389d5c64ecb70e46938e5b424acbb21ea
juan <juan.bisposantos05@gmail.com>
2025-09-09 09:01:17 -0300

    Printando o pacote inteiro não cortado apenas sem os starts e stop bytes

--- app/src/protocols/gt06/utils.py
+++ app/src/protocols/gt06/utils.py
@@ -130,7 +130,7 @@ def format_gt06_packet_for_display(packet_body: bytes) -> str:
            f"  Tamanho Declarado: {length}",
            f"  Serial: {serial}",
            f"  CRC: {hex(crc)}",
-           f"  Corpo (raw): {content_body.hex()}",
+           f"  Corpo (raw): {packet_body.hex()}",
            "--- Detalhes do Conteúdo ---"
        ]
 
