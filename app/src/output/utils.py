9f5d09fdd12910ff4e5deb6d877450d64b321631
juan <juan.bisposantos05@gmail.com>
2025-10-03 11:31:37 -0300

    Add mais uma regra de normalização de IDS

--- app/src/output/utils.py
+++ app/src/output/utils.py
@@ -1,6 +1,7 @@
 
 def normalize_dev_id(dev_id: str) -> str:
     if "803612c9" in dev_id: dev_id = dev_id.replace("803612c9", "")
+    if "00000002" in dev_id: dev_id = dev_id.replace("00000002", "")
     
     dev_id = dev_id.zfill(15)
     
