d56ed26c3dff01761d775671c70256ada4d5df5a
juan <juan.bisposantos05@gmail.com>
2025-10-03 10:00:11 -0300

    Criando um servidor Websocket Simples para streaming de logs da aplicação em tempo real, qualquer cliente pode acessar os logs do servidor, e principalmente acessar os logs POR id de rastreador, o que foi possibilitado pela implementação de tracker id nas linhas de log

---
+++ app/websocket/ws.py
@@ -0,0 +1,82 @@
+import subprocess
+import threading
+from simple_websocket_server import WebSocket
+
+from app.core.logger import get_logger
+
+logger = get_logger(__name__)
+
+class LogStreamer(WebSocket):
+    def __init__(self, server, sock, address):
+        super().__init__(server, sock, address)
+        
+        self.tracker_id = None
+        self.n_lines = None
+        self.log_process = None
+
+    def connected(self):
+        logger.info(f"Cliente conectado: {self.address}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+
+    def handle(self):
+        if self.tracker_id is None:
+            tracker_id_n_lines = self.data
+            self.tracker_id = tracker_id_n_lines.split("|")[0]
+            self.n_lines = tracker_id_n_lines.split("|")[-1]
+
+            logger.info(f"Cliente {self.address} solicitou monitoramento para o rastreador: {self.tracker_id}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+            
+            streaming_thread = threading.Thread(target=self._stream_logs)
+            streaming_thread.daemon = True 
+            streaming_thread.start()
+        else:
+            logger.info(f"Mensagem subsequente de {self.address} ignorada: {self.data}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+
+    def handle_close(self):
+        logger.info(f"Cliente desconectado: {self.address}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+        
+        if self.log_process:
+            logger.info(f"Encerrando o processo journalctl para o cliente {self.address}...", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+            self.log_process.terminate()
+            self.log_process.wait()
+            logger.info("Processo encerrado.", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+
+    def _stream_logs(self):
+        if not self.tracker_id:
+            return
+
+        cmd = [
+            "journalctl", 
+            "-f", 
+            "-u", "gateway-rastreadores.service",
+            "--output=cat",
+            "--all"
+        ]
+
+        # Número de linhas
+        n_lines = self.n_lines if self.n_lines else 100
+        cmd += ["-n", str(n_lines)]
+
+        # Filtra as linhas pelo tracker id
+        cmd += ["|", "grep", str(self.tracker_id)]
+        
+        try:
+            self.log_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            for line in iter(self.log_process.stdout.readline, ''):
+                try:
+                    self.send_message(line.strip())
+                except Exception as e:
+                    logger.info(f"Erro ao enviar para o cliente {self.address}: {e}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+                    break
+            
+            self.log_process.stdout.close()
+            return_code = self.log_process.wait()
+            if return_code:
+                stderr_output = self.log_process.stderr.read()
+                logger.info(f"Subprocesso journalctl terminou com erro. código {return_code}: {stderr_output}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+
+        except Exception as e:
+            logger.info(f"Ocorreu um erro na thread de streaming para {self.address}: {e}", tracker_id="SERVIDOR - WEBSOCKET SERVER")
+        finally:
+            if self.log_process and self.log_process.poll() is None:
+                self.log_process.terminate()
