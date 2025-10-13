370515ee1514b8d8443cf4798e5b88a78c16ee9d
juan <juan.bisposantos05@gmail.com>
2025-10-13 11:50:48 -0300

    Alterando nomeclatura de variaveis

--- app/src/input/satellite/processor.py
+++ app/src/input/satellite/processor.py
@@ -3,11 +3,11 @@ from .. import utils
 from app.src.session.output_sessions_manager import send_to_main_server
 
 def process_packet(data: bytes):
-    hybrid_gsm_dev_id, last_hybrid_location, ign_alert_packet_data, last_serial = mapper.handle_satelite_data(data)
+    output_dev_id, last_merged_location, ign_alert_packet_data, last_serial = mapper.handle_satelite_data(data)
 
-    if last_hybrid_location:
-        utils.log_mapped_packet(last_hybrid_location, "SATELLITAL")
-        send_to_main_server(hybrid_gsm_dev_id, last_hybrid_location, last_serial, data.decode("utf-8"), "SATELLITAL")
+    if last_merged_location:
+        utils.log_mapped_packet(last_merged_location, "SATELLITAL")
+        send_to_main_server(output_dev_id, last_merged_location, last_serial, data.decode("utf-8"), "SATELLITAL")
         
     if ign_alert_packet_data and ign_alert_packet_data.get("universal_alert_id"):
-        send_to_main_server(hybrid_gsm_dev_id, ign_alert_packet_data, last_serial, data.decode("utf-8"), "SATELLITAL", "alert", True)
+        send_to_main_server(output_dev_id, ign_alert_packet_data, last_serial, data.decode("utf-8"), "SATELLITAL", "alert", True)
