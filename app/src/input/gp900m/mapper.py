309e89733baff58176cc428959f0a75680f63db5
Juan Bispo <juan.bisposantos05@gmail.com>
2025-10-28 14:08:33 -0300

    Criando função que decodifica o payload a partir dos bytes da máscara até o final, a lógica de parse foi desenvolvida de acordo com a máscara provida no payload, incrementando a posição do parser de acordo com o tamanho dos campos extraidos

---
+++ app/src/input/gp900m/mapper.py
@@ -0,0 +1,111 @@
+import struct
+from datetime import datetime, timezone
+import json
+import copy
+
+from app.core.logger import get_logger
+from app.services.redis_service import get_redis
+
+from ..utils import handle_ignition_change
+
+logger = get_logger(__name__)
+redis_client = get_redis()
+
+def decode_general_report(payload: bytes):
+
+    data = {}
+    try:
+        mask = payload[:4]
+        parser_at = 4
+        if mask & 0b1: # Product ID present
+            parser_at = 5
+
+        if (mask >> 1) & 0b1: # GPS timestamp present
+            gps_timestamp_bytes = payload[parser_at:parser_at + 4]
+            data["timestamp"] = decode_timestamp(gps_timestamp_bytes)
+
+            parser_at += 4
+
+        if (mask >> 2) & 0b1: # Lat e Lon present
+            lat_long_bytes = payload[parser_at:parser_at + 8]
+            
+            encodedLat, encodedLon = struct.unpack(">II", lat_long_bytes)
+            latitude = (encodedLat / 1000000.0) - 90.0
+            longitude = (encodedLon / 1000000.0) - 180.0
+
+            data["latitude"] = latitude
+            data["longitude"] = longitude
+
+            parser_at += 8
+
+        if (mask >> 3) & 0b1: # Speed And Direction Degrees present
+            speed_degree = payload[parser_at:parser_at + 3]
+
+            speed_kmh, direction = struct.unpack(">BH", speed_degree)
+            
+            data["speed_kmh"] = speed_kmh 
+            data["direction"] = direction
+            
+            parser_at += 3
+
+        if (mask >> 4) & 0b1: # GPS Altitude present
+            parser_at += 2
+
+        if (mask >> 5) & 0b1: # GPS Accuracy present
+            gps_accuracy_byte = payload[parser_at:parser_at + 1]
+
+            # Deslocamos 4 bits para a direita e aplicamos a máscara 0x0F para termos certeza de ter isolado os bits 4-7, depois aplicamos uma máscara para descobrir se o gps está fixado, se os três primeiros bits do nibble forem 001.
+            gps_fixed = (gps_accuracy_byte >> 4) & 0x0F & 0b111 == 1
+            satellites = gps_accuracy_byte & 0x0F
+            
+            data["gps_fixed"] = gps_fixed
+            data["satellites"] = satellites
+
+            parser_at += 2
+
+        if (mask >> 6) & 0b1: # Main Voltage present
+            main_voltage_bytes = parser_at[parser_at:parser_at + 2]
+
+            main_voltage = struct.unpack(">H", main_voltage_bytes)
+            voltage = main_voltage / 1000
+            
+            data["voltage"] = voltage
+
+            parser_at += 2
+
+        if (mask >> 7) & 0b1: # Battery Voltage present
+            parser_at += 2
+
+        if (mask >> 8) & 0b1: # Aux Voltage present
+            parser_at += 2
+
+        if (mask >> 9) & 0b1: # Solar Voltage present
+            parser_at += 2
+
+        if (mask >> 10) & 0b1: # Cellular Service present
+            parser_at += 5
+
+        if (mask >> 11) & 0b1: # RSSI present
+            parser_at += 1
+
+        if (mask >> 12) & 0b1: # GPIO A-D present
+            gpio_ad = payload[parser_at:parser_at + 1]
+
+            parser_at += 1
+
+        if (mask >> 13) & 0b1: # GPIO E-H present
+            gpio_eh = payload[parser_at:parser_at + 1]
+
+            parser_at += 1
+
+        if (mask >> 14) & 0b1: # Odometer present
+            odometer_bytes = payload[parser_at:parser_at + 4]
+
+            odometer = struct.unpack(">I", odometer_bytes)
+
+            data["gps_odometer"] = odometer
+
+        
+    except Exception as e:
+        logger.exception(f"Falha ao decodificar pacote de localização GP900M body_hex={payload.hex()}")
+        return None
