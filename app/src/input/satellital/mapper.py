1b16b9ceec209f5b7671195fa9dd8bee7e698341
Juan Bispo <juan.bisposantos05@gmail.com>
2025-11-06 17:17:00 -0300

    Alterando flag is_realtime para sempre FALSE, pois as posições do satelital nunca chegam  em tempo real

--- app/src/input/satellital/mapper.py
+++ app/src/input/satellital/mapper.py
@@ -129,7 +129,7 @@ def handle_satelite_data(raw_satellite_data: bytes):
         last_merged_location["voltage"] = 2.22
         last_merged_location["satellites"] = 2
         last_merged_location["timestamp"] = parser.parse(satellite_data.get("timestamp"), ignoretz=True)
-        last_merged_location["is_realtime"] = True
+        last_merged_location["is_realtime"] = False
 
         # FIltro de velocidade para ALTAS velocidades
         if speed_filter and speed_filter.isdigit():
