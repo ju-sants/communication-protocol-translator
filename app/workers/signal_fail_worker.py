b3d75bcc93387c02eeebb999e1da797a0a78461f
Juan Bispo <juan.bisposantos05@gmail.com>
2025-11-21 11:39:20 -0300

    Separando worker de falha de sinal em dois muitíssimo importantes: o worker para satelitais puros e um worker para híbridos.

--- app/workers/signal_fail_worker.py
+++ app/workers/signal_fail_worker.py
@@ -13,91 +13,123 @@ redis_client_gateway = get_redis()
 redis_client_data = get_redis(db=3)
 
 def signal_fail_worker():
-    scheduled_work()
+    orchestrator()
 
-    schedule.every(2).hours.do(scheduled_work)
+    schedule.every(2).hours.do(orchestrator)
 
     while True:
         schedule.run_pending()
         time.sleep(60)
 
-def scheduled_work():
+def orchestrator():
     with logger.contextualize(log_label="SIGNAL FAIL WORKER"):
-        try:            
-            tracker_keys = list(redis_client_gateway.scan_iter("tracker:*", count=1000))
+        tracker_keys = list(redis_client_gateway.scan_iter("tracker:*", count=1000))
             
-            failing_trackers_str = set(redis_client_data.smembers("translator_server:failing_trackers"))
-            failing_trackers = [json.loads(fail) for fail in failing_trackers_str]
+        failing_trackers_str = set(redis_client_data.smembers("translator_server:failing_trackers"))
+        failing_trackers = [json.loads(fail) for fail in failing_trackers_str]
+        
+        to_add_to_failing = set()
+        to_remove_from_failing = set()
+
+        with redis_client_gateway.pipeline() as pipe:
+            for key in tracker_keys:
+                pipe.hmget(key, "is_hybrid", "hybrid_id", "last_packet_data", "last_merged_location", "protocol", "mode")
             
-            to_add_to_failing = set()
-            to_remove_from_failing = set()
+            all_trackers_data = pipe.execute()
+
+        for i, tracker_data in enumerate(all_trackers_data):
+                
+            is_hybrid = tracker_data[0]
+            hybrid_id = tracker_data[1]
+            protocol = tracker_data[4]
+            mode = tracker_data[5]
+
+            # Se não for um veículo híbrido, dropamos
+            if not is_hybrid and not hybrid_id:
+                if protocol == "satellital" and mode == "solo":
+                    single_satellites_fail_worker(tracker_keys[i], tracker_data, failing_trackers, to_add_to_failing, to_remove_from_failing)
+                
+                continue
+            else:
+                hybrids_fail_worker(tracker_keys[i], tracker_data, failing_trackers, to_add_to_failing, to_remove_from_failing)
+
+
+        update_failing_trackers_list(to_add_to_failing, to_remove_from_failing)
             
-            with redis_client_gateway.pipeline() as pipe:
-                for key in tracker_keys:
-                    pipe.hmget(key, "is_hybrid", "hybrid_id", "last_packet_data", "last_hybrid_location")
-                
-                all_trackers_data = pipe.execute()
-                
-            for i, tracker_data in enumerate(all_trackers_data):
-                
-                is_hybrid = tracker_data[0]
-                hybrid_id = tracker_data[1]
-                last_gsm_location_str = tracker_data[2]
-                last_hybrid_location_str = tracker_data[3]
+        logger.info(f"Signal fail worker finished. Added: {len(to_add_to_failing)}, Removed: {len(to_remove_from_failing)}")
 
-                # Se não for um veículo híbrido, dropamos
-                if not is_hybrid and not hybrid_id:
-                    continue
+def single_satellites_fail_worker(key: str, tracker_data: tuple, failing_trackers: set, to_add_to_failing: set, to_remove_from_failing: set):
+    last_merged_location_str = tracker_data[3]
+    if not last_merged_location_str:
+            logger.error(f"Pacote de dados inválidos! \n{last_merged_location_str}")
+            return
+    
+    last_merged_location = json.loads(last_merged_location_str)
+    satellite_last_timestamp = last_merged_location.get("timestamp")
 
-                if not last_gsm_location_str or not last_hybrid_location_str:
-                    logger.error(f"Pacotes de dados inválidos! \n{last_hybrid_location_str}\n{last_gsm_location_str}")
-                    continue
+    if satellite_last_timestamp:
+        key = "satellite|" + key # Normalizando keys de satelitais
 
-                last_gsm_location = json.loads(last_gsm_location_str)
-                last_hybrid_location = json.loads(last_hybrid_location_str)
-                
-                gsm_last_timestamp = last_gsm_location.get("timestamp")
-                satellite_last_timestamp = last_hybrid_location.get("timestamp")
-                both_failed = all(
-                                    (utils.is_signal_fail(gsm_last_timestamp), utils.is_signal_fail(satellite_last_timestamp))
-                                )
+        is_signal_fail = utils.is_signal_fail(satellite_last_timestamp)
+        if is_signal_fail and not any(key == fail.get("tracker_label") for fail in failing_trackers):
+            to_add_to_failing.add(key)
+            logger.info(f"Tracker {key} está a mais de 24horas sem comunicar. Marcando para adicionar aos registros de falha.")
 
-                # Se temos uma falha em ambos os rastreadores, adicionamos apenas o registro do satelital (regras específicas de negócio)
-                if both_failed:
-                    packets_to_check = (last_hybrid_location,)
-                else:
-                    packets_to_check = (last_gsm_location, last_hybrid_location)
+        elif not is_signal_fail:
+            if any(key == fail.get("tracker_label") for fail in failing_trackers):
+                to_remove_from_failing.add(key)
+                logger.info(f"Tracker {key} voltou a comunicar. Marcando para retirar dos registros de falha.")
 
-                for packet in packets_to_check:
-                    if packet:
-                        try:
-                            timestamp_str = packet.get("timestamp")
+    
+def hybrids_fail_worker(key: str, tracker_data: tuple, failing_trackers: set, to_add_to_failing: set, to_remove_from_failing: set):
+    try:
+        last_gsm_location_str = tracker_data[2]
+        last_merged_location_str = tracker_data[3]
+
+        if not last_gsm_location_str or not last_merged_location_str:
+            logger.error(f"Pacotes de dados inválidos! \n{last_merged_location_str}\n{last_gsm_location_str}")
+            return
+
+        last_gsm_location = json.loads(last_gsm_location_str)
+        last_merged_location = json.loads(last_merged_location_str)
+        
+        gsm_last_timestamp = last_gsm_location.get("timestamp")
+        satellite_last_timestamp = last_merged_location.get("timestamp")
+        both_failed = all(
+                            (utils.is_signal_fail(gsm_last_timestamp), utils.is_signal_fail(satellite_last_timestamp))
+                        )
+
+        # Se temos uma falha em ambos os rastreadores, adicionamos apenas o registro do satelital (regras específicas de negócio)
+        if both_failed:
+            packets_to_check = (last_merged_location,)
+        else:
+            packets_to_check = (last_gsm_location, last_merged_location)
+
+        for packet in packets_to_check:
+            if packet:
+                try:
+                    timestamp_str = packet.get("timestamp")
+                    
+                    if timestamp_str:
+                        if float(packet.get("voltage", 0.0)) == 2.22 and int(packet.get("satellites", 0)) == 2:
+                            key = "satellite|" + key
+
+                        is_signal_fail = utils.is_signal_fail(timestamp_str)
+                        if is_signal_fail and not any(key == fail.get("tracker_label") for fail in failing_trackers):
+                            to_add_to_failing.add(key)
+                            logger.info(f"Tracker {key} está a mais de 24horas sem comunicar. Marcando para adicionar aos registros de falha.")
+
+                        elif not is_signal_fail:
+                            if any(key == fail.get("tracker_label") for fail in failing_trackers):
+                                to_remove_from_failing.add(key)
+                                logger.info(f"Tracker {key} voltou a comunicar. Marcando para retirar dos registros de falha.")
                             
-                            if timestamp_str:
-                                key = tracker_keys[i]
-                                if float(packet.get("voltage", 0.0)) == 2.22 and int(packet.get("satellites", 0)) == 2:
-                                    key = "satellite|" + key
 
-                                is_signal_fail = utils.is_signal_fail(timestamp_str)
-                                if is_signal_fail and not any(key == fail.get("tracker_label") for fail in failing_trackers):
-                                    to_add_to_failing.add(key)
-                                    logger.info(f"Tracker {key} está a mais de 24horas sem comunicar. Marcando para adicionar aos registros de falha.")
+                except (json.JSONDecodeError, KeyError, TypeError) as e:
+                    logger.error(f"Error processing tracker data for key {key}: {e}")
 
-                                elif not is_signal_fail:
-                                    if any(key == fail.get("tracker_label") for fail in failing_trackers):
-                                        to_remove_from_failing.add(key)
-                                        logger.info(f"Tracker {key} voltou a comunicar. Marcando para retirar dos registros de falha.")
-                                    
-
-                        except (json.JSONDecodeError, KeyError, TypeError) as e:
-                            logger.error(f"Error processing tracker data for key {tracker_keys[i]}: {e}")
-
-            update_failing_trackers_list(to_add_to_failing, to_remove_from_failing)
-                
-            logger.info(f"Signal fail worker finished. Added: {len(to_add_to_failing)}, Removed: {len(to_remove_from_failing)}")
-
-        except Exception as e:
-            logger.error(f"An error occurred in the signal fail worker: {e}")
+    except Exception as e:
+        logger.error(f"An error occurred in the signal fail worker: {e}")
 
 
 def update_failing_trackers_list(to_add_to_failing, to_remove_from_failing):
