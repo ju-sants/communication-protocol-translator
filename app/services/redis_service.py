db1678d290b205ead8c8deaa3bcbc83fa6ab33ab
Juan Bispo <juan.bisposantos05@gmail.com>
2025-11-27 09:40:31 -0300

    Utilizando a robustez de connection pool para redis

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -15,10 +15,11 @@ def get_redis(db: int | None = None, host: str | None = None, port: int | None =
     host = host if host is not None else settings.REDIS_HOST
     port = port if port is not None else settings.REDIS_PORT
     password = password if password is not None else settings.REDIS_PASSWORD
-    logger.info(f"Connecting to Redis DB {db} at {host}:{port} with password {'set' if password else 'not set'}", log_label="SERVIDOR")
+    logger.info(f"Connecting to Redis DB {db} at {host}:{port} with ConnectionPool", log_label="SERVIDOR")
 
     try:
-        redis_conn = redis.Redis(db=db, host=host, port=port, password=password, decode_responses=True)
+        connection_pool = redis.ConnectionPool(max_connections=20, db=db, host=host, port=port, password=password, decode_responses=True)
+        redis_conn = redis.Redis(connection_pool=connection_pool)
         redis_conn.ping()
         logger.info(f"Successfully connected to Redis DB {db} at {host}:{port}", log_label="SERVIDOR")
     except redis.ConnectionError as e:
