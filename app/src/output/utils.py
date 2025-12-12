f15ebf9d4c443e802c8ee9b5b9c19307d1352478
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-12 10:50:30 -0300

    Adicionando uma regra de normalização aos ids em "normalize_dev_id"

--- app/src/output/utils.py
+++ app/src/output/utils.py
@@ -1,6 +1,7 @@
 
 def normalize_dev_id(dev_id: str) -> str:
     if "803612c9" in dev_id: dev_id = dev_id.replace("803612c9", "")
+    if "803912c9" in dev_id: dev_id = dev_id.replace("803912c9", "")
     if "00000002" in dev_id: dev_id = dev_id.replace("00000002", "")
     if "80360001" in dev_id: dev_id = dev_id.replace("80360001", "")
     
